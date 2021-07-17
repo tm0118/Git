9b948308138598aa880dc1f8abadbe2ad9bf0c8e
tm0118 <taemingim69@gmail.com>
2021-07-18 00:50:28 +0900

    git

--- python/1차원 배열/1차원배열7.py
+++ python/1차원 배열/1차원배열7.py
@@ -1,8 +1,12 @@
+
 number = int(input())
 for i in range(number):
     number_list = list(map(int,input().split()))
     average = sum(number_list[1:]) / int(len(number_list)-1)
-    print(average)
-    
-    
+    count = 0
+    for j in number_list[1:]:
+        if j>average:
+            count += 1
+    k = count / (len(number_list)-1) *100
+    print("%.3f%%"%k)
     
