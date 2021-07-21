f0571ce150725b3918a8fd2dd7ed98b041846487
tm0118 <taemingim69@gmail.com>
2021-07-21 17:39:03 +0900

    github

--- python/문자열/문자열2.py
+++
@@ -1,6 +0,0 @@
-n = input()
-nums = input()
-total = 0
-for i in nums :
-    total += int(i)  # total= total+int(i)
-print(total)
--- python/문자열/문자열4.py
+++
@@ -1,7 +0,0 @@
-num = int(input())
-for i in range(num):
-        su, s = input().split()
-        text = ""
-        for i in s:
-                text += int(su)*i
-        print(text)
