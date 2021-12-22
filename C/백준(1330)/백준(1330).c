422d033ae9ac34ad9eeaeff4024015677ae4a796
godtaemin <taemin6697@gmail.com>
2021-12-22 21:29:14 +0900

    -all

--- C/백준(1330)/백준(1330).c
+++ C/백준(1330)/백준(1330).c
@@ -1,29 +1,22 @@
-#include<stdio.h>
-int main(){
-    int line;
-    int i,j;
-    int num = 1;
-    int count = 1;
-    printf("라인 수를 입력하시오:");
-    scanf("%d",&line);
-    int k = line;
-    for(i=1;i<=line;i++){
-        for(j=k-1;j>0;j--){
-            printf(" ");
-        }
-        for(j=0;j<i*2-1;j++){
-                if(num>9){
-                    num = 0;
-                }
-            if(j%2==0){
-            printf("%d",num);
-            num++;
-            }
-            else{
-                printf(" ");
-            }
-        }
-        printf("\n");
-        k--;
-    }
+#define _CRT_SECURE_NO_WARININGS
+#include <stdio.h>
+int main() {
+
+    char x;
+    printf("문자를 입력하세요:");
+    scanf("%c", &x);
+
+
+
+    if (x == 'c')
+        printf("Circle");
+    else if (x == 't')
+        printf("Triangle");
+    else if (x == 'r')
+        printf("Rectangle");
+    else
+        printf("Unknown");
+
+
+    return 0;
 }
